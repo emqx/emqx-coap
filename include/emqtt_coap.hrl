7cd4203cb9c63fbf3c73182c82287cb13d0a725d
Feng <ery.lee@gmail.com>
2016-07-06 08:37:36 +0800

    response code

--- include/emqtt_coap.hrl
+++ include/emqtt_coap.hrl
@@ -14,9 +14,56 @@
 %% limitations under the License.
 %%--------------------------------------------------------------------
 
+%% CON: Confirmable
+%% NON: Non-confirmable
+%% ACK: Acknowledgement
+%% RST: Rest
+-type(coap_type() :: 'CON' | 'NON' | 'ACK' | 'RST').
+
 -type(coap_method() :: 'GET' | 'POST' | 'PUT' | 'DELETE').
+
+-type(coap_option() :: 'Etag' | ).
 
 -record(coap_message, {type, method, code, id, token = <<>>, options = [], payload = <<>>}).
 
 -type(coap_message() :: #coap_message{}).
+
+%% Get response: 2.05(Content), 2.03(Valid),
+%% Post response: 2.01(Created), 2.04(Changed), 2.02(Deleted)
+%% Put: 2.04(Changed), 2.01(Created)
+%% Delete: 2.02(Deleted)
+%%
+
+%%--------------------------------------------------------------------
+%% Response Code
+%%--------------------------------------------------------------------
+
+%% Success 2.xx (5.9.1)
+-define(RC_CREATED, {2, 01}).
+-define(RC_DELETED, {2, 02}).
+-define(RC_VALID,   {2, 03}).
+-define(RC_CHANGED, {2, 04}).
+-define(RC_CONTENT, {2, 05}).
+
+%% Client Error 4.xx (5.9.2)
+-define(RC_BAD_REQUEST,  {4, 00}).
+-define(RC_UNAUTHORIZED, {4, 01}).
+
+%%   4.02 Bad Option
+%%   4.03 Fobidden
+%%   4.04 Not Found
+%%   4.05 Method Not Allowed
+%%   4.06 Not Acceptable
+%%   4.12 Precondition Failed
+%%   4.13 Request Entity Too Large
+%%   4.15 Unsupported Content-format
+
+%% Server Error 5.xx
+%%   5.00 Internal Server Erro34 BVD
+%%   5.01 Not Implemented
+%%   5.02 Bad Gateway
+%%   5.03 Service Unavailable
+%%   5.04 Gateway Timeout
+%%   5.05 Proxying Not Supported
+
 
